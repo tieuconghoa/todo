195989918e189236eb9c2b1fc20c068384caad27
tieuconghoa <tieuconghoa@luvina.net>
2022-11-03 16:43:17 +0700

    commit sidebar

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,85 +1,149 @@
 <template>
-  <div class="header">
-    <div class="top-bar">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <div class="float-left">
-              <a href="#" class=""><span class="mr-2 fa fa-envelope"></span>
-                <span class="d-none d-md-inline-block">info@yourdomain.com</span></a>
-              <span class="mx-md-2 d-inline-block"></span>
-              <a href="#" class=""><span class="mr-2 fa fa-phone"></span>
-                <span class="d-none d-md-inline-block">1+ (234) 5678 9101</span></a>
-            </div>
-            <div class="float-right">
-              <a href="#" class=""><i class="mr-2 fa-brands fa-twitter"></i>
-                <span class="d-none d-md-inline-block">Twitter</span></a>
-              <span class="mx-md-2 d-inline-block"></span>
-              <a href="#" class=""><span class="mr-2 fa-brands fa-facebook-f"></span>
-                <span class="d-none d-md-inline-block">Facebook</span></a>
+  <div>
+    <div class="header">
+      <div class="top-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="float-left">
+                <a href="#" class=""
+                  ><span class="mr-2 fa fa-envelope"></span>
+                  <span class="d-none d-md-inline-block"
+                    >info@yourdomain.com</span
+                  ></a
+                >
+                <span class="mx-md-2 d-inline-block"></span>
+                <a href="#" class=""
+                  ><span class="mr-2 fa fa-phone"></span>
+                  <span class="d-none d-md-inline-block"
+                    >1+ (234) 5678 9101</span
+                  ></a
+                >
+              </div>
+              <div class="float-right">
+                <a href="#" class=""
+                  ><i class="mr-2 fa-brands fa-twitter"></i>
+                  <span class="d-none d-md-inline-block">Twitter</span></a
+                >
+                <span class="mx-md-2 d-inline-block"></span>
+                <a href="#" class=""
+                  ><span class="mr-2 fa-brands fa-facebook-f"></span>
+                  <span class="d-none d-md-inline-block">Facebook</span></a
+                >
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <header class="main-header">
+        <div class="mid-header d-lg-flex d-none">
+          <div class="logo-branch">
+            <div>SHOPPING.COM</div>
+          </div>
+          <div class="icon-menu">
+            <span>
+              <button class="btn"><i class="fa fa-user"></i></button>
+            </span>
+            <span>
+              <button class="btn" data-toggle="modal" data-target="#myModal2">
+                <i class="fa fa-search"></i>
+              </button>
+            </span>
+            <span>
+              <button class="btn"><i class="fa fa-cart-shopping"></i></button>
+            </span>
+          </div>
+        </div>
+        <div class="list-menu">
+          <nav class="navbar navbar-expand-lg navbar-light container">
+            <button
+              class="navbar-header navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="true"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Chủ đề 1</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Chủ đề 2</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Chủ đề 3</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Chủ đề 4</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Dropdown link
+                  </a>
+                  <div
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="d-lg-none d-flex form-search-mobile">
+              <input type="text" class="form-control" />
+              <button class="btn btn-nav"><i class="fa fa-search"></i></button>
+              <button class="btn"><i class="fa fa-cart-shopping"></i></button>
+            </div>
+          </nav>
+        </div>
+      </header>
     </div>
-    <header class="main-header">
-      <div class="mid-header d-lg-flex d-none">
-        <div class="logo-branch">
-          <div>SHOPPING.COM</div>
-        </div>
-        <div class="icon-menu">
-          <span>
-            <button class="btn"><i class="fa fa-user"></i></button>
-          </span>
-          <span>
-            <button class="btn"><i class="fa fa-search"></i></button>
-          </span>
-          <span>
-            <button class="btn"><i class="fa fa-cart-shopping"></i></button>
-          </span>
-        </div>
-      </div>
-      <div class="list-menu">
-        <nav class="navbar navbar-expand-lg navbar-light container">
-          <button class="navbar-header navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Chủ đề 1</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Chủ đề 2</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Chủ đề 3</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Chủ đề 4</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                  aria-haspopup="true" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-            </ul>
+    <div
+      class="modal right fade"
+      id="myModal2"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="myModalLabel2"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel2">TÌM KIẾM</h4>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
-          <div class="d-lg-none d-flex form-search-mobile">
-            <input type="text" class="form-control">
-            <button class="btn btn-nav"><i class="fa fa-search"></i></button>
-            <button class="btn"><i class="fa fa-cart-shopping"></i></button>
+
+          <div class="modal-body">
+            <form action="#" class="form-inline">
+              <input type="text" class="form-control">
+              <button class="btn btn-danger"><i class="fa fa-search"></i></button>
+            </form>
           </div>
-        </nav>
+        </div>
+        <!-- modal-content -->
       </div>
-    </header>
+      <!-- modal-dialog -->
+    </div>
   </div>
 </template>
 <script>
@@ -87,12 +151,12 @@ export default {
   created() {
     document.addEventListener("scroll", () => {
       if (window.scrollY > 0) {
-        $(".header").addClass("scrollable")
+        $(".header").addClass("scrollable");
       } else {
-        $(".header").removeClass("scrollable")
+        $(".header").removeClass("scrollable");
       }
-    })
-  }
+    });
+  },
 };
 </script>
 
@@ -109,7 +173,7 @@ export default {
   justify-content: center;
 }
 
-.mid-header>div {
+.mid-header > div {
   flex: 1 0 0;
 }
 
@@ -134,7 +198,7 @@ export default {
   left: 0;
   right: 0;
   z-index: 999;
-  animation: fadeInDown 0.5s both
+  animation: fadeInDown 0.5s both;
 }
 
 .scrollable .mid-header {
@@ -155,6 +219,47 @@ export default {
   top: 10px;
   right: 0px;
   width: 80%;
+}
+.modal.right .modal-dialog {
+  position: fixed;
+  margin: auto;
+  width: 320px;
+  height: 100%;
+  -webkit-transform: translate3d(0%, 0, 0);
+  -ms-transform: translate3d(0%, 0, 0);
+  -o-transform: translate3d(0%, 0, 0);
+  transform: translate3d(0%, 0, 0);
+}
+
+.modal.right .modal-content {
+  height: 100%;
+  overflow-y: auto;
+}
+
+.modal.right .modal-body {
+  padding: 15px 15px 80px;
+}
+.modal.right.fade .modal-dialog {
+  right: 0;
+  -webkit-transition: opacity 0.3s linear, right 0.3s ease-out;
+  -moz-transition: opacity 0.3s linear, right 0.3s ease-out;
+  -o-transition: opacity 0.3s linear, right 0.3s ease-out;
+  transition: opacity 0.3s linear, right 0.3s ease-out;
+}
+
+.modal.right.fade.in .modal-dialog {
+  right: 0;
+}
+
+/* ----- MODAL STYLE ----- */
+.modal-content {
+  border-radius: 0;
+  border: none;
+}
+
+.modal-header {
+  margin-top:  20px;
+  border: none;
 }
 
 @keyframes fadeInDown {
