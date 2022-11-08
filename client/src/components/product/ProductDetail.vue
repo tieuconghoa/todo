a702d5fa17585c65975fa7098fe071f09f2903fb
tieuconghoa <tieuconghoa193@gmail.com>
2022-11-08 22:57:23 +0700

    update layout

--- client/src/components/product/ProductDetail.vue
+++ client/src/components/product/ProductDetail.vue
@@ -1,483 +1,138 @@
 <template>
-  <div class="container">
-    <!-- product -->
-    <div class="product-content product-wrap clearfix product-deatil">
-      <div class="row">
-        <div class="col-md-6 col-sm-12 col-xs-12">
-          <div class="product-image">
-            <div id="myCarousel-2" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                <li data-target="#myCarousel-2" data-slide-to="0" class=""></li>
-                <li
-                  data-target="#myCarousel-2"
-                  data-slide-to="1"
-                  class="active"
-                ></li>
-                <li data-target="#myCarousel-2" data-slide-to="2" class=""></li>
-              </ol>
-              <div class="carousel-inner">
-                <!-- Slide 1 -->
-                <div class="carousel-item active">
-                  <img
-                    src="https://via.placeholder.com/700x400/FFB6C1/000000"
-                    class="img-responsive"
-                    alt=""
-                  />
-                </div>
-                <!-- Slide 2 -->
-                <div class="carousel-item">
-                  <img
-                    src="https://via.placeholder.com/700x400/87CEFA/000000"
-                    class="img-responsive"
-                    alt=""
-                  />
-                </div>
-                <!-- Slide 3 -->
-                <div class="carousel-item">
-                  <img
-                    src="https://via.placeholder.com/700x400/B0C4DE/000000"
-                    class="img-responsive"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <a
-                class="carousel-control-prev"
-                href="#myCarousel-2"
-                data-slide="prev"
-              >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a
-                class="carousel-control-next"
-                href="#myCarousel-2"
-                data-slide="next"
-              >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div>
-          </div>
-        </div>
+  <div class="small-container single-product">
+    <div class="row">
+      <div class="col-6">
+        <img src="https://i.ibb.co/1MydDzN/gallery-1.jpg" alt="" width="100%" id="ProductImg" />
 
-        <div class="col-md-5 col-md-offset-1 col-sm-12 col-xs-12">
-          <h2 class="name">
-            Product Name Title Here
-            <small>Product by <a href="javascript:void(0);">Adeline</a></small>
-          </h2>
-          <hr />
-          <h3 class="price-container">
-            $129.54
-            <small>*includes tax</small>
-          </h3>
-          <div class="certified">
-            <ul>
-              <li>
-                <a href="javascript:void(0);"
-                  >Delivery time<span>7 Working Days</span></a
-                >
-              </li>
-              <li>
-                <a href="javascript:void(0);"
-                  >Certified<span>Quality Assured</span></a
-                >
-              </li>
-            </ul>
+        <div class="small-img-row">
+          <div class="small-img-col">
+            <img src="https://i.ibb.co/1MydDzN/gallery-1.jpg" alt="" width="100%" class="small-img" @click="changeImg"/>
           </div>
-          <hr />
-          <div class="description description-tabs">
-            <ul id="myTab" class="nav nav-justified nav-pills">
-              <li class="nav-item active">
-                <a href="#more-information" data-toggle="tab" class="nav-link active no-margin"
-                  >Product Description
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#specifications" class="nav-link" data-toggle="tab">Specifications</a>
-              </li>
-            </ul>
-            <div id="myTabContent" class="tab-content">
-              <div class="tab-pane fade show active" id="more-information">
-                <br />
-                <strong>Description Title</strong>
-                <p>
-                  Integer egestas, orci id condimentum eleifend, nibh nisi
-                  pulvinar eros, vitae ornare massa neque ut orci. Nam aliquet
-                  lectus sed odio eleifend, at iaculis dolor egestas. Nunc
-                  elementum pellentesque augue sodales porta. Etiam aliquet
-                  rutrum turpis, feugiat sodales ipsum consectetur nec.
-                </p>
-              </div>
-              <div class="tab-pane fade" id="specifications">
-                <br />
-                <dl class="">
-                  <dt>Gravina</dt>
-                  <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
-                  <dd>Donec id elit non mi porta gravida at eget metus.</dd>
-                  <dd>Eget lacinia odio sem nec elit.</dd>
-                  <br />
-
-                  <dt>Test lists</dt>
-                  <dd>A description list is perfect for defining terms.</dd>
-                  <br />
-
-                  <dt>Altra porta</dt>
-                  <dd>Vestibulum id ligula porta felis euismod semper</dd>
-                </dl>
-              </div>
-            </div>
+          <div class="small-img-col">
+            <img src="https://i.ibb.co/4j34KdX/gallery-2.jpg" alt="" width="100%" class="small-img" @click="changeImg"/>
           </div>
-          <hr />
-          <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-6">
-              <a href="javascript:void(0);" class="btn btn-success btn-lg"
-                >Add to cart ($129.54)</a
-              >
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6">
-              <div class="btn-group pull-right">
-                <button class="btn btn-white btn-default">
-                  <i class="fa fa-star"></i> Add to wishlist
-                </button>
-                <button class="btn btn-white btn-default">
-                  <i class="fa fa-envelope"></i> Contact Seller
-                </button>
-              </div>
-            </div>
+          <div class="small-img-col">
+            <img src="https://i.ibb.co/kGhWJ84/gallery-3.jpg" alt="" width="100%" class="small-img" @click="changeImg"/>
+          </div>
+          <div class="small-img-col">
+            <img src="https://i.ibb.co/4j34KdX/gallery-2.jpg" alt="" width="100%" class="small-img" @click="changeImg"/>
           </div>
         </div>
       </div>
+      <div class="col-6">
+        <p>Home / T-Shirt</p>
+        <h1>Red Printed T-Shirt by HRX</h1>
+        <h4>₹500</h4>
+        <select>
+          <option>Select Size</option>
+          <option>XXL</option>
+          <option>XL</option>
+          <option>L</option>
+          <option>M</option>
+          <option>S</option>
+        </select>
+       <div class="form-inline row">
+        <input class="" type="number" value="1" />
+        <a href="" class="btn btn-card col-3 offset-2">Add To Cart</a>
+       </div>
+
+        <h3>Product Details</h3>
+        <br />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias,
+          accusamus soluta quasi illum, deserunt ut iste rem obcaecati
+          inventore, est harum repellendus fuga velit odio sint officia
+          corrupti eum perspiciatis.
+        </p>
+      </div>
     </div>
-    <!-- end product -->
   </div>
 </template>
-<style scoped>
-.product-content {
-  /* border: 1px solid #dfe5e9; */
-  margin-bottom: 20px;
-  margin-top: 12px;
-  background: #fff;
+<script>
+export default {
+  methods : {
+    changeImg(event) {
+      document.getElementById('ProductImg').src = event.target.src;
+    }
+  },
+}
+</script>
+<style>
+
+.col-2 img {
+  max-width: 100%;
+  padding: 50px 0;
 }
 
-.product-content .carousel-control.left {
-  margin-left: 0;
+.col-2 h1 {
+  font-size: 50px;
+  line-height: 60px;
+  margin: 25px 0;
 }
 
-.product-content .product-image {
-  background-color: #fff;
+.small-container {
+  max-width: 1080px;
+  margin: auto;
+  padding-left: 25px;
+  padding-right: 25px;
+}
+
+.single-product {
+  margin-top: 80px;
+}
+
+.single-product .col-2 img {
+  padding: 0;
+}
+
+.single-product .col-2 {
+  padding: 20px;
+}
+
+.single-product h4 {
+  margin: 20px 0;
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.single-product select {
   display: block;
-  min-height: 238px;
-  overflow: hidden;
-  position: relative;
-}
-
-.product-content .product-deatil {
-  border-bottom: 1px solid #dfe5e9;
-  padding-bottom: 17px;
-  padding-left: 16px;
-  padding-top: 16px;
-  position: relative;
-  background: #fff;
-}
-
-.product-content .product-deatil h5 a {
-  color: #2f383d;
-  font-size: 15px;
-  line-height: 19px;
-  text-decoration: none;
-  padding-left: 0;
-  margin-left: 0;
-}
-
-.product-content .product-deatil h5 a span {
-  color: #9aa7af;
-  display: block;
-  font-size: 13px;
-}
-
-.product-content .product-deatil span.tag1 {
-  border-radius: 50%;
-  color: #fff;
-  font-size: 15px;
-  height: 50px;
-  padding: 13px 0;
-  position: absolute;
-  right: 10px;
-  text-align: center;
-  top: 10px;
-  width: 50px;
-}
-
-.product-content .product-deatil span.sale {
-  background-color: #21c2f8;
-}
-
-.product-content .product-deatil span.discount {
-  background-color: #71e134;
-}
-
-.product-content .product-deatil span.hot {
-  background-color: #fa9442;
-}
-
-.product-content .description {
-  font-size: 12.5px;
-  line-height: 20px;
-  padding: 10px 14px 16px 19px;
-  background: #fff;
-}
-
-.product-content .product-info {
-  padding: 11px 19px 10px 20px;
-}
-
-.product-content .product-info a.add-to-cart {
-  color: #2f383d;
-  font-size: 13px;
-  padding-left: 16px;
-}
-
-.product-content name.a {
-  padding: 5px 10px;
-  margin-left: 16px;
-}
-
-.product-info.smart-form .btn {
-  padding: 6px 12px;
-  margin-left: 12px;
-  margin-top: -10px;
-}
-
-.product-entry .product-deatil {
-  border-bottom: 1px solid #dfe5e9;
-  padding-bottom: 17px;
-  padding-left: 16px;
-  padding-top: 16px;
-  position: relative;
-}
-
-.product-entry .product-deatil h5 a {
-  color: #2f383d;
-  font-size: 15px;
-  line-height: 19px;
-  text-decoration: none;
-}
-
-.product-entry .product-deatil h5 a span {
-  color: #9aa7af;
-  display: block;
-  font-size: 13px;
-}
-
-.load-more-btn {
-  background-color: #21c2f8;
-  border-bottom: 2px solid #037ca5;
-  border-radius: 2px;
-  border-top: 2px solid #0cf;
+  padding: 10px;
   margin-top: 20px;
-  padding: 9px 0;
-  width: 100%;
 }
 
-.product-block .product-deatil p.price-container span,
-.product-content .product-deatil p.price-container span,
-.product-entry .product-deatil p.price-container span,
-.shipping table tbody tr td p.price-container span,
-.shopping-items table tbody tr td p.price-container span {
-  color: #21c2f8;
-  font-family: Lato, sans-serif;
-  font-size: 24px;
-  line-height: 20px;
+.single-product input {
+  width: 50px;
+  height: 38px;
+  padding-left: 15px;
+  font-size: 20px;
+  border: 1px solid #ff523b;
 }
 
-.product-info.smart-form .rating label {
-  margin-top: 0;
+input:focus {
+  outline: none;
 }
 
-.product-wrap .product-image span.tag2 {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  width: 36px;
-  height: 36px;
-  border-radius: 50%;
-  padding: 10px 0;
+.small-img-row {
+  display: flex;
+  justify-content: space-between;
+}
+
+.small-img-col {
+  flex-basis: 24%;
+  cursor: pointer;
+}
+
+.small-img-col img {
+  width: auto;
+  height: 100px;
+}
+
+.btn-card {
+  display: inline-block;
+  background: #ff523b;
   color: #fff;
-  font-size: 11px;
-  text-align: center;
-}
-
-.product-wrap .product-image span.sale {
-  background-color: #57889c;
-}
-
-.product-wrap .product-image span.hot {
-  background-color: #a90329;
-}
-
-.shop-btn {
-  position: relative;
-}
-
-.shop-btn > span {
-  background: #a90329;
-  display: inline-block;
-  font-size: 10px;
-  box-shadow: inset 1px 1px 0 rgba(0, 0, 0, 0.1),
-    inset 0 -1px 0 rgba(0, 0, 0, 0.07);
-  font-weight: 700;
-  border-radius: 50%;
-  padding: 2px 4px 3px !important;
-  text-align: center;
-  line-height: normal;
-  width: 19px;
-  top: -7px;
-  left: -7px;
-}
-
-.description-tabs {
-  padding: 30px 0 5px !important;
-}
-
-.description-tabs .tab-content {
-  padding: 10px 0;
-}
-
-.product-deatil {
-  padding: 30px 30px 50px;
-}
-
-.product-deatil hr + .description-tabs {
-  padding: 0 0 5px !important;
-}
-
-.product-deatil .carousel-control.left,
-.product-deatil .carousel-control.right {
-  background: none !important;
-}
-
-.product-deatil .glyphicon {
-  color: #3276b1;
-}
-
-.product-deatil .product-image {
-  border-right: none !important;
-}
-
-.product-deatil .name {
-  margin-top: 0;
-  margin-bottom: 0;
-}
-
-.product-deatil .name small {
-  display: block;
-}
-
-.product-deatil .name a {
-  margin-left: 0;
-}
-
-.product-deatil .price-container {
-  font-size: 24px;
-  margin: 0;
-  font-weight: 300;
-}
-
-.product-deatil .price-container small {
-  font-size: 12px;
-}
-
-.product-deatil .fa-2x {
-  font-size: 16px !important;
-}
-
-.product-deatil .fa-2x > h5 {
-  font-size: 12px;
-  margin: 0;
-}
-
-.product-deatil .fa-2x + a,
-.product-deatil .fa-2x + a + a {
-  font-size: 13px;
-}
-
-.profile-message ul {
-  list-style: none;
-}
-
-.product-deatil .certified {
-  margin-top: 10px;
-}
-
-.product-deatil .certified ul {
-  padding-left: 0;
-}
-
-.product-deatil .certified ul li:not(first-child) {
-  margin-left: -3px;
-}
-
-.product-deatil .certified ul li {
-  display: inline-block;
-  background-color: #f9f9f9;
-  border: 1px solid #ccc;
-  padding: 13px 19px;
-}
-
-.product-deatil .certified ul li:first-child {
-  border-right: none;
-}
-
-.product-deatil .certified ul li a {
-  text-align: left;
-  font-size: 12px;
-  color: #6d7a83;
-  line-height: 16px;
-  text-decoration: none;
-}
-
-.product-deatil .certified ul li a span {
-  display: block;
-  color: #21c2f8;
-  font-size: 13px;
-  font-weight: 700;
-  text-align: center;
-}
-
-.product-deatil .message-text {
-  width: calc(100% - 70px);
-}
-
-@media only screen and (min-width: 1024px) {
-  .product-content div[class*="col-md-4"] {
-    padding-right: 0;
-  }
-  .product-content div[class*="col-md-8"] {
-    padding: 0 13px 0 0;
-  }
-  .product-wrap div[class*="col-md-5"] {
-    padding-right: 0;
-  }
-  .product-wrap div[class*="col-md-7"] {
-    padding: 0 13px 0 0;
-  }
-  .product-content .product-image {
-    border-right: 1px solid #dfe5e9;
-  }
-  .product-content .product-info {
-    position: relative;
-  }
-}
-
-.message img.online {
-  width: 40px;
-  height: 40px;
+  margin: 30px 10px;
+  transition: backgound 0.5s;
 }
 </style>
